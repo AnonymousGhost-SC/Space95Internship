2428eb16ee57c7c3930d21af1571b153aa5c6896
kaikum009 <159820021+kaikum009@users.noreply.github.com>
2025-03-04 03:04:47 +0400

    Create FE-102-MAR-2025.js
    first session of FE-102

---
+++ FE-102-MAR-2025.js
@@ -0,0 +1,35 @@
+// session-1-learnings
+
+// Date functions are built-in functions in programming languages that allow you to work with dates and times. 
+// They help in performing operations like getting the current date, formatting dates, adding or subtracting days, and comparing dates.
+
+new Date() → Creates a new date object
+Date.now() → Returns the current timestamp (milliseconds)
+toDateString() → Converts a date to a readable format
+getFullYear() → Returns the year
+
+An object is a collection of related data (properties/attributes) and functions (methods/behaviors) that work together. 
+Objects are a key concept in object-oriented programming (OOP) and help structure code in a more modular and reusable way.
+
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2023,
+    start: function() {
+    console.log("Car is starting...");
+    }
+};
+
+// Accessing properties
+console.log(car.brand);  // Output: Toyota
+
+// Calling methods
+car.start();  // Output: Car is starting...
+
+// operations on array
+1.looping through an array
+2.push operations
+3.pull operations
+4.shift and unshift operations
+
+// so in the first session they again went through the fundamentals of javascript
